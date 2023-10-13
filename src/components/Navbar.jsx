f3dbc41a9d0201c12dd5a2d170baae44bdf01648
Javokhir <jtursunboev@bk.ru>
2023-10-13 09:30:20 +0900

    navbar is done

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import { Mail, Notifications, Pets } from "@mui/icons-material";
+import {
+  AppBar,
+  Avatar,
+  Badge,
+  Box,
+  InputBase,
+  styled,
+  Toolbar,
+  Typography,
+} from "@mui/material";
+import React from "react";
 
+const StyleToolBar = styled(Toolbar)({
+  display: "flex",
+  justifyContent: "space-between",
+});
+
+const Search = styled("div")(({ theme }) => ({
+  backgroundColor: "white",
+  padding: "0 10px",
+  borderRadius: theme.shape.borderRadius,
+  width: "40%",
+}));
+const Icons = styled(Box)(({ theme }) => ({
+  display: "none",
+  alignItems: "center",
+
+  [theme.breakpoints.up("sm")]: {
+    display: "flex",
+    gap: "20px",
+  },
+}));
+const UserBox = styled(Box)(({ theme }) => ({
+  display: "flex",
+  gap: "10px",
+  alignItems: "center",
+
+  [theme.breakpoints.up("sm")]: {
+    display: "none",
+  },
+}));
 function Navbar() {
   return (
-    <div>Navbar</div>
-  )
+    <Box>
+      <AppBar position="sticky">
+        <StyleToolBar>
+          <Typography
+            variant="h6"
+            sx={{
+              display: { xs: "none", sm: "block" },
+            }}
+          >
+            PicPost
+          </Typography>
+          <Pets
+            sx={{
+              display: {
+                xs: "block",
+                sm: "none",
+              },
+            }}
+          />
+          <Search>
+            <InputBase placeholder="search..." />
+          </Search>
+          <Icons>
+            <Badge badgeContent={4} color="error">
+              <Mail />
+            </Badge>
+            <Badge badgeContent={2} color="error">
+              <Notifications />
+            </Badge>
+            <Avatar
+              sx={{ width: "30px", height: "30px" }}
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+          </Icons>
+          <UserBox>
+            <Avatar
+              sx={{ width: "30px", height: "30px" }}
+              src="https://images.pexels.com/photos/91227/pexels-photo-91227.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
+            />
+            <Typography variant="span">John</Typography>
+          </UserBox>
+        </StyleToolBar>
+      </AppBar>
+    </Box>
+  );
 }
 
-export default Navbar
+export default Navbar;
